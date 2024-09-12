24ee9dd2dd035c3c7db1891051fee07119d922a4
username Anda <arifprasojo999@email.com>
2024-09-12 09:46:16 +0700

    Pertemuan 3

---
+++ lab3/bin/TugasLatihan.dart
@@ -0,0 +1,19 @@
+void main(){
+  String namalengkap =  "Arif Prasojo";
+  int NIM = 2241760100;
+  String Sigma = "Sigma";
+
+   for (int i = 0; i <= 201; i++) {
+    if (isPrime(i)) {
+      print("Bilangan Prima: $i - $namalengkap - $NIM - $Sigma",);
+    }
+  }
+}
+
+bool isPrime(int number) {
+  if (number <= 1) return false; 
+  for (int i = 2; i <= number ~/ 2; i++) {
+    if (number % i == 0) return false; 
+  }
+  return true;
+}
